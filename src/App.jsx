6410464c6f6e5a12ed6d0b4503160e2336d85ca9
Francesco-Zucco <zuccoqwe@gmail.com>
2024-06-27 19:28:49 +0300

    added scroll restore

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="bg-[#161617] max-w-[2560px] flex flex-col ">
+      <ScrollRestoration />
       <NavBar />
       <Outlet />
       <Footer />
